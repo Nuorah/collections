2e700111b92b6786071bb4be52def9c209926361
Haroun Moatassime <haroun.moatassime@gmail.com>
2026-02-04 09:10:59 +0100

    Added vec and started bounding volume hierarchy

---
+++ src/bounding_volume_hierarchy.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+const Vec3 = @import("vec.zig").Vec3;
+
+const Box = struct {
+    min: Vec3,
+    max: Vec3,
+};
+
+const Triangle = struct {
+    points: [3]Vec3,
+};
+
+const NodeData = union(enum) {
+    leaf: []Triangle,
+    internal: struct { left: *Node, right: *Node },
+};
+
+const Node = struct {
+    data: NodeData,
+    box: Box,
+};
+
+pub const BoundingVolumeHierarchy = struct {};
